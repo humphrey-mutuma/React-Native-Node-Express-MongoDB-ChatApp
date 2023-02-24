6eb34fe07e59ba9ff7b285f41367aa5d1bca92b8
humphrey-mutuma <humphreymutuma01@gmail.com>
2023-02-24 21:05:28 +0300

    export static data

---
+++ data.ts
@@ -0,0 +1,137 @@
+export const chats = [
+  {
+    recipientName: "Alex Mutuma",
+    message: "Yolooo there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Janiie Mellissa",
+    message: "hello there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    recipientName: "Dr Tatus",
+    message: "Wharuuup chief",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Joe the trader",
+    message: "This is craiizy",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    recipientName: "Alexii Mutuma",
+    message: "Yolooo there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Janet Mellissa",
+    message: "hello there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    recipientName: "Mrs Tirus",
+    message: "Wharuuup chief",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Josee Dev",
+    message: "This is craiizy",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    recipientName: "Ken Mutuma",
+    message: "Yolooo there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Jane Mellissaa",
+    message: "hello there",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    recipientName: "Mr Tiruss",
+    message: "Wharuuup chief",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    recipientName: "Joe  Dev",
+    message: "This is craiizy",
+    recipientImage:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+];
+
+export const users = [
+  {
+    name: "Alex Mutuma",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Janiie Mellissa",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    name: "Dr Tatus",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Joe the trader",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    name: "Alei Mutuma",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Janet Mellissa",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    name: "Mrs Tirus",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Josee Dev",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    name: "Ken Mutuma",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Jane Mellissaa",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+  {
+    name: "Mr Tiruss",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  },
+  {
+    name: "Joe  Dev",
+    userAvatar:
+      "https://cdn.pixabay.com/photo/2017/02/04/12/25/man-2037255__340.jpg",
+  },
+];
